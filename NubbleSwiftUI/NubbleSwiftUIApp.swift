80f2aa752fc20757249e664ad643f09cf30159f8
willperes <thewidocs@gmail.com>
2024-05-05 20:04:52 -0300

    Initial Commit

---
+++ NubbleSwiftUI/NubbleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NubbleSwiftUIApp.swift
+//  NubbleSwiftUI
+//
+//  Created by Willian Peres on 05/05/24.
+//
+
+import SwiftUI
+
+@main
+struct NubbleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
